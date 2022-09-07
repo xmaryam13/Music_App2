93f1b751fa69aebf089b72e9078f9dae1ba43ee4
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-07 20:59:03 +0100

    Update musicapp_client1.py

--- musicapp_client1.py
+++ musicapp_client1.py
@@ -81,7 +81,7 @@ def musicWindow():
     listBox = Listbox(window, height = 10, width = 39, activestyle = 'dotbox',bg = 'LightSkyBlue', borderwidth = 2, font = ('Calibri',10))
     listBox.place(x=12,y=20)
     
-for file in os.listdir('shared_files'):
+    for file in os.listdir('shared_files'):
     filename = os.fsdecode(file)
     listbox.insert(song_counter,filename)
     song_counter = song_counter + 1
