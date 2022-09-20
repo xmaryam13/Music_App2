0807971f5cfb865a6f5ef1cb3d85945f49035077
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-20 20:40:58 +0100

    Update musicapp_server1.py

--- musicapp_server1.py
+++ musicapp_server1.py
@@ -2,6 +2,9 @@ import socket
 from threading import Thread
 import time
 import os
+from pyftpdlib.authorizers import DummyAuthorizer 
+from pyftpdlib.handlers import FTPHandler 
+from pyftpdlib.servers import FTPServer
 
 IP_ADDRESS = '127.0.0.1'
 PORT = 8050
@@ -58,6 +61,27 @@ def set_up():
     
 setup_thread = Thread(target=set_up)
 setup_thread.start()
+
+    
+def ftp():
+    global IP_ADDRESS
+
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("lftpd","lftpd",".",perm="elradfmw")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    ftp_server = FTPServer((IP_ADDRESS,21),handler)
+    ftp_server.serve_forever()
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
+
+
+ftp_thread = Thread(target=ftp)
+ftp_thread.start()
+        
 
     
         
